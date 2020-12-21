31a8214d306ee8e8c51047f09f293816dce85ec5
Thibor <thibor@vp.pl>
2020-12-21 05:30:05 +0000

    update

--- jsRapStar.js
+++ jsRapStar.js
@@ -1,57 +1,57 @@
-(function($){
-	$.fn.jsRapStar = function(options){
-		
-		return this.each(function(){
+(function ($) {
+	$.fn.jsRapStar = function (options) {
+
+		return this.each(function () {
 			let value = $(this).attr('value');
 			let color = $(this).attr('color');
 			value = value === undefined ? 0 : parseFloat(value);
 			color = color === undefined ? 'yellow' : color;
 			this.opt = $.extend({
-				star:'&#9733',
-				colorFront:color,
-				colorBack:'white',
-				enabled:true,
-				step:true,
-				starHeight:32,
-				length:6,
+				star: '&#9733',
+				colorFront: color,
+				colorBack: 'white',
+				enabled: true,
+				step: true,
+				starHeight: 32,
+				length: 6,
 				value: value,
-				onClick:null,
-				onMousemove:null,
-				onMouseleave:null
-			},options);
+				onClick: null,
+				onMousemove: null,
+				onMouseleave: null
+			}, options);
 			var base = this;
 			var starH = Array(this.opt.length + 1).join('<span>' + this.opt.star + '</span>');
-			this.StarB = $(this).addClass('rapStar').css({color:this.opt.colorBack,'font-size':this.opt.starHeight + 'px'}).html(starH);
+			this.StarB = $(this).empty().addClass('rapStar').css({ color: this.opt.colorBack, 'font-size': this.opt.starHeight + 'px' }).html(starH);
 			var sw = this.StarB.width() / this.opt.length;
 			var aw = base.opt.value * sw;
-			this.StarF = $('<div>').addClass('rapStarFront').css({color:this.opt.colorFront}).html(starH).width(aw).appendTo(this);
-			if(this.opt.enabled){
+			this.StarF = $('<div>').addClass('rapStarFront').css({ color: this.opt.colorFront }).html(starH).width(aw).appendTo(this);
+			if (this.opt.enabled) {
 				$(this).bind({
-					mousemove:function(e){
+					mousemove: function (e) {
 						e.preventDefault();
 						var relativeX = e.clientX - $(base)[0].getBoundingClientRect().left;
 						var e = Math.floor(relativeX / sw) + 1;
-						if(base.opt.step) newWidth = e * sw;
+						if (base.opt.step) newWidth = e * sw;
 						else newWidth = relativeX;
 						this.StarF.width(newWidth);
-						if(base.opt.onMousemove)
-							base.opt.onMousemove.call(base,newWidth / sw);
+						if (base.opt.onMousemove)
+							base.opt.onMousemove.call(base, newWidth / sw);
 					},
-					mouseleave:function(e){
+					mouseleave: function (e) {
 						this.StarF.width(aw);
-						if(base.opt.onMouseleave)
-							base.opt.onMouseleave.call(base,base.opt.value);
+						if (base.opt.onMouseleave)
+							base.opt.onMouseleave.call(base, base.opt.value);
 					},
-					click:function(e){
+					click: function (e) {
 						e.preventDefault();
 						aw = newWidth;
 						this.StarF.width(newWidth);
 						base.opt.value = newWidth / sw;
-						if(base.opt.onClick)
-							base.opt.onClick.call(base,base.opt.value);
+						if (base.opt.onClick)
+							base.opt.onClick.call(base, base.opt.value);
 					}
 				});
-			}else
+			} else
 				$(this).addClass('rapStarDisable');
 		})
 	}
