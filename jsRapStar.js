a6069dbd5c52cacbe9b13c3d077c6f9e00e6ad47
Thibor <thibor@vp.pl>
2021-01-18 03:58:49 +0000

    working bootstrap modal

--- jsRapStar.js
+++ jsRapStar.js
@@ -2,8 +2,8 @@
 	$.fn.jsRapStar = function (options) {
 
 		return this.each(function () {
-			let value = $(this).attr('value');
-			let color = $(this).attr('color');
+			let value = $(this).attr('value'); 
+			let color = $(this).attr('color'); 
 			value = value === undefined ? 0 : parseFloat(value);
 			color = color === undefined ? 'yellow' : color;
 			this.opt = $.extend({
@@ -21,23 +21,24 @@
 			let starH = Array(this.opt.length + 1).join('<span>' + this.opt.star + '</span>');
 			$(this).empty().addClass('rapStar').css({ color: this.opt.colorBack, 'font-size': this.opt.starHeight + 'px' }).html(starH);
 			let widthValue = ($(this).width() * this.opt.value) / this.opt.length;
+			let widthCurrent = widthValue;
 			this.StarF = $('<div>').addClass('rapStarFront').css({ color: this.opt.colorFront }).html(starH).width(widthValue).appendTo(this);
 			if (this.opt.enabled) {
 				$(this).bind({
 					mousemove: function (e) {
 						widthCurrent = e.clientX - this.getBoundingClientRect().left;
 						if (this.opt.step)
-							widthCurrent = Math.floor((widthCurrent * this.opt.length) / $(this).width()) + 1;
+							widthCurrent = (Math.floor((widthCurrent * this.opt.length) / $(this).width()) + 1) * ($(this).width() / this.opt.length);
 						this.StarF.width(widthCurrent);
 						if (this.opt.onMousemove)
-							this.opt.onMousemove.call(this, (widthCurrent * this.opt.length) / $(this).width());
+							this.opt.onMousemove.call(this, widthCurrent /  ($(this).width() / this.opt.length));
 					},
 					mouseleave: function (e) {
 						this.StarF.width(widthValue);
 					},
 					click: function (e) {
 						widthValue = widthCurrent;
-						this.opt.value = (widthValue * this.opt.length) / $(this).width();
+						this.opt.value = widthValue / ($(this).width() / this.opt.length);
 						if (this.opt.onClick)
 							this.opt.onClick.call(this, this.opt.value);
 					}
